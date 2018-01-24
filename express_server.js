30134ba476decf7dd3a97d266e10437daa5be430
Brandon Stranzl <brandonstranzl@gmail.com>
2018-01-24 20:50:03 +0000

    commit of tinyurl with update and delete requests - CRUDgit add .git add .

---
+++ express_server.js
@@ -0,0 +1,81 @@
+var express = require("express");
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.set("view engine", "ejs");
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.end("Hello!\n");
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = {urls: urlDatabase};
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id, urls: urlDatabase };
+  res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+});
+
+app.post("/urls/:id", (req, res) => {
+  urlDatabase[req.params.id] = req.body.longURL;
+  res.redirect("/urls");
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = urlDatabase[req.params.shortURL]
+  res.redirect(longURL);
+});
+
+app.post("/urls/:id/delete", (req, res) => {
+  delete urlDatabase[req.params.id];
+  res.redirect("/urls");
+});
+
+app.get("/hello", (req, res) => {
+  res.end("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
+function generateRandomString() {
+  var letters = ['a','b','c','d','e','f','g','h','i','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+  var numbers = [0,1,2,3,4,5,6,7,8,9];
+  var randomstring = '';
+
+  for(var i=0;i<5;i++){
+  var rlet = Math.floor(Math.random()*letters.length);
+  randomstring += letters[rlet];
+  }
+  for(var i=0;i<3;i++){
+  var rnum = Math.floor(Math.random()*numbers.length);
+  randomstring += numbers[rnum];
+  }
+
+  return (randomstring);
+
+  }
